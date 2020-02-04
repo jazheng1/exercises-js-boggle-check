b76f3dd5990174ee0dd44d56b742c7d45b3bf03e
Jason Zheng <jazheng@davidson.edu>
2020-02-04 13:52:17 -0800

    added working shake function into boggle.js

--- boggle.js
+++ boggle.js
@@ -7,6 +7,15 @@ function newBoggleBoard() {
   ];
 }
 
+function shuffle(array) {
+  for (let x = 0; x < 16; x++) {
+    let y = Math.floor(Math.random() * (x + 1));
+    [array[x], array[y]] = [array[y], array[x]];
+  }
+}
+
+// source for shuffle: http://javascript.info/task/shuffle
+
 /**
  * Prints out a boggle board.
  */
@@ -21,9 +30,48 @@ function printBoggleBoard(boggleBoard) {
  */
 function shake(boggleBoard) {
   // This is your job. :)
+  let arrayOfDice = [
+    'AAEEGN',
+    'ELRTTY',
+    'AOOTTW',
+    'ABBJOO',
+    'EHRTVW',
+    'CIMOTU',
+    'DISTTY',
+    'EIOSST',
+    'DELRVY',
+    'ACHOPS',
+    'HIMNQU',
+    'EEINSU',
+    'EEGHNW',
+    'AFFKPS',
+    'HLNNRZ',
+    'DEILRX',
+  ];
+
+  let letterArray = [];
+  for (let i = 0; i < arrayOfDice.length; i++) {
+    for (let j = 0; j < 1; j++) {
+      let randomLetter = arrayOfDice[i].charAt(Math.floor(Math.random() * arrayOfDice[i].length));
+      if (randomLetter === 'Q') {
+        randomLetter = 'Qu';
+      }
+      letterArray.push(randomLetter);
+    }
+  }
+
+  shuffle(letterArray);
+
+  let counter = 0;
+  for (let z = 0; z < boggleBoard.length; z++) {
+    for (let y = 0; y < boggleBoard[z].length; y++) {
+      boggleBoard[z][y] = letterArray[counter];
+      counter++;
+    }
+  }
+
   return boggleBoard;
 }
-
 let board = newBoggleBoard();
 
 shake(board);
